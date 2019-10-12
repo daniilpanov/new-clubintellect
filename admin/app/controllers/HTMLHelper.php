8be87357c2929f322615401e3f43a1218728c44d
daniilpanov <daniil_panov2005@mail.ru>
2019-10-12 14:54:35 +0300

    Commit before replacing html-code in body to html-helper

--- admin/app/controllers/HTMLHelper.php
+++ admin/app/controllers/HTMLHelper.php
@@ -8,6 +8,8 @@ use admin\app\Factory;
 
 class HTMLHelper extends Controller
 {
+    private $html_model = null;
+
     public function begin()
     {
         Factory::createModel("HTMLDocument", null, false, "title", "ru");
@@ -15,11 +17,16 @@ class HTMLHelper extends Controller
 
     public function head()
     {
+        return $this->html_model->createHead();
+    }
 
+    public function body()
+    {
+        $this->html_model->renderHead()->createBody();
     }
 
     public function end()
     {
-
+        $this->html_model->renderBody()->rendering();
     }
 }
--- admin/index.php
+++ admin/index.php
@@ -6,15 +6,15 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="Author" content="<Клуб интеллектуалов" />
+    <meta name="Author" content="Клуб интеллектуалов">
     <!--End Meta-->
 
     <title>Система управления сайтом</title>
 
     <!--CSS-->
     <link href="style/bootstrap.min.css" rel="stylesheet" media="screen">
-    <link href="style/style.css" rel="stylesheet" />
-    <link href="style/jquery.lightbox-0.5.css" rel="stylesheet" />
+    <link href="style/style.css" rel="stylesheet">
+    <link href="style/jquery.lightbox-0.5.css" rel="stylesheet">
     <!--End CSS-->
 
     <!--Fonts-->
